ab96c6ec73131e8f3e36b85c6d401b615a09a433
Lambros Petrou <lambrospetrou@gmail.com>
2017-02-11 19:02:54 +0000

    Refactored the goroutines to be instantiated at the beginning and communicating through messages.

--- goimpl/src/main.go
+++ goimpl/src/main.go
@@ -17,29 +17,6 @@ func check(e error) {
 
 type Empty struct{}
 
-type Ngram struct {
-	Value string
-	Valid bool
-}
-
-type Result struct {
-	NgramValue Ngram
-	Pos        int
-}
-
-type ByAppearance []Result
-
-func (a ByAppearance) Len() int      { return len(a) }
-func (a ByAppearance) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
-func (a ByAppearance) Less(i, j int) bool {
-	if a[i].Pos < a[j].Pos {
-		return true
-	} else if a[i].Pos > a[j].Pos {
-		return false
-	}
-	return len(a[i].NgramValue.Value) < len(a[j].NgramValue.Value)
-}
-
 func readInitial(in *bufio.Reader, ngdb *NgramDB) *NgramDB {
 	for {
 		line, _ := in.ReadString('\n')
@@ -76,6 +53,55 @@ func printResults(results []NgramResult) {
 	fmt.Println()
 }
 
+type WorkerJob struct {
+	Ngdb        *NgramDB
+	Doc         string
+	StartIdxEnd int
+	ResultCh    chan []NgramResult
+}
+
+type WorkerStatus struct {
+	Status string
+}
+
+type WorkerPool struct {
+	Workers        int
+	WorkerJobCh    []chan WorkerJob
+	WorkerStatusCh []chan WorkerStatus
+}
+
+func NewWorkerPool() *WorkerPool {
+	numWorkers := runtime.NumCPU()
+
+	pool := &WorkerPool{
+		Workers:        numWorkers,
+		WorkerJobCh:    make([]chan WorkerJob, 0, numWorkers),
+		WorkerStatusCh: make([]chan WorkerStatus, 0, numWorkers),
+	}
+	for i := 0; i < numWorkers; i++ {
+		pool.WorkerJobCh = append(pool.WorkerJobCh, make(chan WorkerJob, 1))
+		pool.WorkerStatusCh = append(pool.WorkerStatusCh, make(chan WorkerStatus, 1))
+
+		go worker(pool.WorkerStatusCh[i], pool.WorkerJobCh[i])
+	}
+
+	return pool
+}
+
+func worker(chStatus chan WorkerStatus, chJobIn chan WorkerJob) {
+	for {
+		select {
+		case job := <-chJobIn:
+			job.ResultCh <- partialQueryDoc(job.Ngdb, job.Doc, job.StartIdxEnd)
+			break
+		case msg := <-chStatus:
+			if msg.Status == "terminate" {
+				return
+			}
+		}
+	}
+}
+
 func partialQueryDoc(ngdb *NgramDB, doc string, startIdxEnd int) []NgramResult {
 	localResults := make([]NgramResult, 0, 16)
 
@@ -104,20 +130,19 @@ func partialQueryDoc(ngdb *NgramDB, doc string, startIdxEnd int) []NgramResult {
 	return localResults
 }
 
-func queryDoc(ngdb *NgramDB, doc string) {
+func queryDoc(ngdb *NgramDB, wpool *WorkerPool, doc string) {
 	//fmt.Fprintln(os.Stderr, "queryDoc", len(ngrams))
 	docSz := len(doc)
 
-	// single thread
-	//results := partialQueryDoc(ngdb, doc, docSz, docSz)
+	cores := wpool.Workers
+	chans := make([]chan []NgramResult, 0, cores)
 
-	cores := runtime.NumCPU()
-	chans := make([]chan []NgramResult, 0, cores+1)
+	currentWorker := 0
 
 	maxLenNgram := 100
 
 	start := 0
-	batchSz := int(docSz / cores)
+	batchSz := int(docSz/cores) + 1
 	for start < docSz {
 		startIdxEnd := int(math.Min(float64(start+batchSz), float64(docSz)))
 		// make sure we are at the end of a word
@@ -131,24 +156,26 @@ func queryDoc(ngdb *NgramDB, doc string) {
 		chRes := make(chan []NgramResult)
 		chans = append(chans, chRes)
 
-		go func(ngdb *NgramDB, partialDoc string, startIdxEnd int, chOut chan []NgramResult) {
-			chRes <- partialQueryDoc(ngdb, partialDoc, startIdxEnd)
-			close(chRes)
-		}(ngdb, doc[start:docEnd], startIdxEnd-start, chRes)
+		wpool.WorkerJobCh[currentWorker] <- WorkerJob{
+			Ngdb:        ngdb,
+			Doc:         doc[start:docEnd],
+			StartIdxEnd: startIdxEnd - start,
+			ResultCh:    chRes,
+		}
 
 		start = startIdxEnd
+		currentWorker += 1
 	}
 
 	results := make([]NgramResult, 0, 128)
 	for _, ch := range chans {
-		partialResults := <-ch
-		results = append(results, partialResults...)
+		results = append(results, (<-ch)...)
 	}
 
 	printResults(results)
 }
 
-func processWorkload(in *bufio.Reader, ngdb *NgramDB) *NgramDB {
+func processWorkload(in *bufio.Reader, ngdb *NgramDB, workerPool *WorkerPool) *NgramDB {
 	for {
 		line, err := in.ReadString('\n')
 		if err != nil {
@@ -165,7 +192,7 @@ func processWorkload(in *bufio.Reader, ngdb *NgramDB) *NgramDB {
 		} else if line[0] == 'A' {
 			ngdb.AddNgram(line[2:])
 		} else if line[0] == 'Q' {
-			queryDoc(ngdb, line[2:])
+			queryDoc(ngdb, workerPool, line[2:])
 		} else if line[0] == 'F' {
 			// TODO process the batch
 			continue
@@ -182,11 +209,13 @@ func main() {
 	ngramDb := NewNgramDB()
 	ngdb := &ngramDb
 
+	workerPool := NewWorkerPool()
+
 	in := bufio.NewReader(os.Stdin)
 
 	ngdb = readInitial(in, ngdb)
 	fmt.Fprintf(os.Stderr, "initial ngrams 1st level [%d]\n", len(ngdb.Trie.Root.Children))
 
-	ngdb = processWorkload(in, ngdb)
+	ngdb = processWorkload(in, ngdb, workerPool)
 	fmt.Fprintf(os.Stderr, "after workload ngrams 1st level [%d]\n", len(ngdb.Trie.Root.Children))
 }
