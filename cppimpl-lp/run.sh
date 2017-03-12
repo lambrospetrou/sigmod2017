a247d0d8e8ae01a9c5050a382d21246bff37a515
Lambros Petrou <lambrospetrou@gmail.com>
2017-03-12 23:00:03 +0000

    CPU Affinity to avoid the threads being on the same core.

--- cppimpl-lp/run.sh
+++ cppimpl-lp/run.sh
@@ -5,9 +5,22 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 NUM_THREADS=$( grep -e "siblings" /proc/cpuinfo | sed 's/^.*: \([0-9]\+\)$/\1/' | head -n1 )
 NUM_THREADS=2
 
+#THREAD_AFFINITIES="0,2,"
+THREAD_AFFINITIES=""
+i=0
+while [ "$i" -lt "$NUM_THREADS" ]; do
+    TA="$TA$(($i * 2)),"
+    i=$(($i + 1))
+done
+
+THREAD_AFFINITIES="$TA"
+
+
 # OpenMP configuration
 export OMP_NUM_THREADS="$NUM_THREADS"
 export OMP_PROC_BIND=TRUE
+export GOMP_CPU_AFFINITY="$THREAD_AFFINITIES"
+
 export OMP_WAIT_POLICY=ACTIVE
 #export OMP_WAIT_POLICY=PASSIVE
 
